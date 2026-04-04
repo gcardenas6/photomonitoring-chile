e54e3044ad9c3f3435cabf0d4af2fa9fa412025f
Gustavo Cárdenas <gustavocardenas@uc.cl>
2026-04-04 18:02:32 -0300

    added data fix excel

--- data_cleaning.R
+++ data_cleaning.R
@@ -25,8 +25,19 @@ full_df <- do.call(rbind, lapply(files, function(file) {
 # Fix colnames ### THIS IS UGLY, FIX THE ORIGINAL COLNAMES LATER
 colnames(full_df)[2:6] <- c("classification", "correct", "notes", "error_type", "file_path") 
 
+# Delete errors
+df_correct <- subset(full_df, correct == 1)
 
+# Create an excel file with all unique categories
+data_fix <- data.frame(classification = sort(unique(df_correct$classification)),
+                       sci_name = character(length = length(sort(unique(df_correct$classification)))),
+                       tax_level = character(length = length(sort(unique(df_correct$classification)))),
+                       notes = character(length = length(sort(unique(df_correct$classification)))),
+                       protected_areas = sapply(sort(unique(df_correct$classification)), function(x) {
+                         paste(unique(df_correct$protected_area[df_correct$classification == x]), collapse = ", ")
+                       }))
 
+write.xlsx(data_fix, "data_fix.xlsx")
 
 
 
